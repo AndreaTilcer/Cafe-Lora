5aeba80e79f0b817d7f78be7d669fc57c818efdf
Andrea Tilcerová <andrea.tilcer@gmail.com>
2022-04-30 11:43:54 +0200

    Objednávání

--- src/index.js
+++ src/index.js
@@ -13,3 +13,22 @@ for (let i = 0; i < zabalMenu.length; i += 1) {
     document.querySelector('nav').classList.add('nav-closed');
   });
 }
+
+let ordered = false;
+
+const objednatBtn = document.querySelector('.order-btn');
+objednatBtn.addEventListener('click', () => {
+  if (ordered === false) {
+    objednatBtn.textContent = 'Zrušit';
+    document.querySelector('.drink__cup').classList.add('drink__cup--selected');
+    ordered = true;
+    console.log(ordered);
+  } else {
+    objednatBtn.textContent = 'Objednat';
+    document
+      .querySelector('.drink__cup')
+      .classList.remove('drink__cup--selected');
+    ordered = false;
+    console.log(ordered);
+  }
+});
